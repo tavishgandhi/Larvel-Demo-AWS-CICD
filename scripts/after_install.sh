09ea1f82f9232ad0829808b860d0d3b53e174466
Tavish <tavishgandhi4@gmail.com>
2021-08-25 17:30:57 +0530

    Updating file location to laravel

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Set permissions to storage and bootstrap cache
-sudo chmod -R 0777 /var/www/html/storage
-sudo chmod -R 0777 /var/www/html/bootstrap/cache
+sudo chmod -R 0777 /var/www/html/laravel/storage
+sudo chmod -R 0777 /var/www/html/laravel/bootstrap/cache
 #
-cd /var/www/html
+cd /var/www/html/laravel
 #
 # Run composer
 sudo composer install
 
 # Copy the env file
-cp /var/www/.env .
+sudo cp /var/www/.env .
 
 php artisan migrate
 
